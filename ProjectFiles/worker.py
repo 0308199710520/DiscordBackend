f1a1a74b08256e57371ebd0c15ec8b88f6640236
Michael Poole <michaelpoole10@gmail.com>
2023-08-01 00:37:46 +0100

    setup precommit

--- ProjectFiles/worker.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass, field
-
-@dataclass
-class Worker():
-    
-    pass
-    
-    
--- UnitTests/test_worker.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-from ProjectFiles.worker import Worker
-
-class test_worker(unittest.TestCase):
-    pass
