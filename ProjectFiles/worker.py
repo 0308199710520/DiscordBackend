c1e3028c83464b68ea98819a22250ac800b35e17
Michael Poole <michaelpoole10@gmail.com>
2023-08-01 00:24:27 +0100

    initial commit with worker initialisation

---
+++ ProjectFiles/worker.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class Worker():
+    
+    pass
+    
+    
---
+++ UnitTests/test_worker.py
@@ -0,0 +1,6 @@
+import unittest
+
+from ProjectFiles.worker import Worker
+
+class test_worker(unittest.TestCase):
+    pass
